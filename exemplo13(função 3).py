1386e58730ba14dfeead64d7f54c0c23e2b66cb9
Bruno Costa <abscosta@yahoo.com.br>
2021-02-22 14:56:00 -0300

    Adicionado aulas sobre função em python

---
+++ exemplo13(função 3).py
@@ -0,0 +1,13 @@
+def calculaImposto(sal, imposto = False):
+    if imposto:
+        desconto = float(input("Qual a porcentagem do imposto: "))
+    else:
+        desconto = 20
+    
+    desconto = desconto/100
+    return sal - (sal * desconto)
+
+salario = float(input("Informe seu salário: "))
+
+
+print(f"Seu salário com desconto é: R${calculaImposto(salario,True)}")
