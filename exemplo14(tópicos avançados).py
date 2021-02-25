95d9675cbe3d3f3af7d8092d6f0f5abcb5fe60c2
Bruno Costa <abscosta@yahoo.com.br>
2021-02-25 14:10:29 -0300

    Adicionado exemplo da função filter() no arquivo sobre tópicos avançados

--- exemplo14(tópicos avançados).py
+++ exemplo14(tópicos avançados).py
@@ -13,7 +13,15 @@ print(numeros)
 soma = lambda x,y:x + y
 
 print(soma(4,5))
-
+# Trabalhando com função map()
 dobro = list(map(lambda x:x*2,lista))
 
 print(dobro)
+
+# Trabalhando com a função filter
+valores = list(range(20,61))
+print(valores)
+
+pares = list(filter(lambda num:num%2==0,valores))
+
+print("Os números pares são: \n",pares)
